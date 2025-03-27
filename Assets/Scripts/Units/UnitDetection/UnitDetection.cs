b05ae4da4618903f14b90e046b48291bcc185aed
RavenLauncelot <launcelotraven@gmail.com>
2025-03-27 17:00:55 +0000

    Unit detection

--- Assets/Scripts/Units/UnitDetection/UnitDetection.cs
+++ Assets/Scripts/Units/UnitDetection/UnitDetection.cs
@@ -8,7 +8,13 @@ public class UnitDetection : MonoBehaviour
     
     public void searchArea()
     {
-        
+        Collider[] inRange = Physics.OverlapSphere(transform.position, searchRadius);
+
+        //Unit is within range lets see if they can be spotted (they arent behind a wall)
+        foreach (Collider c in inRange)
+        {
+            if (Physics.Raycast())
+        }
     }
 
     //if hasnt been detected will go back to being undetected
