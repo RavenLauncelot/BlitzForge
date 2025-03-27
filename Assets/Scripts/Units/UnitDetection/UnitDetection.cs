0a41a425d7860b17aee492d849541e14bdbe394f
RavenLauncelot <launcelotraven@gmail.com>
2025-03-27 16:59:56 +0000

    UNit detection

---
+++ Assets/Scripts/Units/UnitDetection/UnitDetection.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using Unity.Physics;
+
+public class UnitDetection : MonoBehaviour
+{
+    bool canSearch;
+    float searchRadius;
+    
+    public void searchArea()
+    {
+        
+    }
+
+    //if hasnt been detected will go back to being undetected
+    public void detectionRefresh()
+    {
+
+    }
+}
+
+public struct isDetected
+{
+    
+}
